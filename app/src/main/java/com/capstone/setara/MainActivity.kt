e5e8c31d14d23e722c110cfce5f02440fcd9a3aa
faizallyusuf <faizalyusuf297@gmail.com>
2024-11-23 07:54:11 +0700

    penambahan asset menu

--- app/src/main/java/com/capstone/setara/MainActivity.kt
+++ app/src/main/java/com/capstone/setara/MainActivity.kt
@@ -7,12 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.setara.databinding.ActivityMainBinding
 import com.capstone.setara.view.SignInActivity
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
 
 lateinit var auth: FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationBar: NavigationBarView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
