259690e45cc3e5bae257099d05077406b8aa7eff
lilisapr <lilisapriah630@gmail.com>
2024-11-23 13:38:18 +0700

    perubahan logo aplikasi part 2

--- app/src/main/java/com/capstone/setara/MainActivity.kt
+++ app/src/main/java/com/capstone/setara/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navigationBar: NavigationBarView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
